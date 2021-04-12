4464371bf34afb6bb508b1583bf46311136585f1
Raghav Goel <raghavgoel847@gmail.com>
2021-04-13 03:42:18 +0530

    Final Committ

--- src/components/Graphs.js
+++ src/components/Graphs.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import "../styles/Graphs.css";
+import { GraphsRead } from '../utils/utils';
 import Graphitem from './Graphitem';
 
 
@@ -10,45 +11,42 @@ function Graphs() {
             <h1>Graphs</h1>
             <div className="graph__container1">
                 <div className="graph__section"> 
-                   <Graphitem  title="Temperature" link="https://thingspeak.com/channels/1336379/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"/>
+                   <Graphitem  title="Temperature" link={GraphsRead(1)}/>
                 </div>
                 <div className="graph__section"> 
-                   <Graphitem title= "Ultrasonic" link="https://thingspeak.com/channels/1336379/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"/>
+                   <Graphitem title= "Ultrasonic" link={GraphsRead(2)}/>
                 </div>
              
                 <div className="graph__section"> 
-                   <Graphitem title= "Light-Button" link=" https://thingspeak.com/channels/1336379/charts/3?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"/>
+                   <Graphitem title= "Light-Button" link={GraphsRead(3)}/>
                 </div>
             </div>
 
             <div className="graph__container2">
                 <div className="graph__section"> 
-                   <Graphitem  title="Photo-Resistor" link="https://thingspeak.com/channels/1336379/charts/4?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"/>
+                   <Graphitem  title="Photo-Resistor" link={GraphsRead(4)}/>
                 </div>
                 <div className="graph__section"> 
-                   <Graphitem title= "Pir-Sensor" link="https://thingspeak.com/channels/1336379/charts/5?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"/>
+                   <Graphitem title= "Pir-Sensor" link={GraphsRead(5)}/>
                 </div>
              
                 <div className="graph__section"> 
-                   <Graphitem title= "Intruder-Button" link=" https://thingspeak.com/channels/1336379/charts/6?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"/>
+                   <Graphitem title= "Intruder-Button" link={GraphsRead(6)}/>
                 </div>
             </div>
 
              <div className="graph__container3">
                 <div className="graph__section"> 
-                   <Graphitem  title="Buzzer" link="https://thingspeak.com/channels/1336379/charts/7?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"/>
+                   <Graphitem  title="Buzzer" link={GraphsRead(7)}/>
                 </div>
                 <div className="graph__section"> 
-                   <Graphitem title= "Gas-Sensor" link="https://thingspeak.com/channels/1336379/charts/8?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"/>
+                   <Graphitem title= "Gas-Sensor" link={GraphsRead(8)}/>
                 </div>
              
                
             </div>
            
             
-            <div>
-            hello
-            </div>
         </div>
     )
 }
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -4,3 +4,6 @@ export const ThingSpeakWriteAPI = (field) => `update?api_key=FW8F6DEGWF2P9CEG&${
 
 export const ThingSpeakReadAPI = (field_number) =>
   `channels/1336379/fields/${field_number}/last?api_key=RX065Q6TIP3VQZMK`;
+
+export const GraphsRead = (field_number) => 
+`https://thingspeak.com/channels/1336379/charts/${field_number}?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15`;
